50e2c5d0f610727965b2d6ea18af183829de6c7b
Bhavya Luthra <bhavyaluthra1234@gmail.com>
2026-01-07 13:13:05 +0530

    Render React flow with basic input & output nodes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ReactFlow } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
+// creation of nodes and edges
+const nodes = [
+  {
+    id: "input",
+    position: { x: 100, y: 100 },
+    data: { label: "Input Node" },
+  },
+  {
+    id: "output",
+    position: { x: 400, y: 100 },
+    data: { label: "Output Node" },
+  },
+];
+
+// connector between nodes
+const edges = [
+  {
+    id: "input-output",
+    source: "input",
+    target: "output",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <ReactFlow nodes={nodes} edges={edges} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
